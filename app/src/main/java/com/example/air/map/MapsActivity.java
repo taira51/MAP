4cd61bde7d4e387497749d07cd80db4fd9a64bc7
taira51 <b3256@oic.jp>
2016-11-13 03:15:10 +0900

    xmlで細かい初期設定を追加

--- app/src/main/java/com/example/air/map/MapsActivity.java
+++ app/src/main/java/com/example/air/map/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.air.map;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.location.Criteria;
+import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,6 +13,7 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -28,37 +32,59 @@ public class MapsActivity extends FragmentActivity implements LocationListener,O
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //MapsInitializerの初期化（例外処理してない）
+        MapsInitializer.initialize(this);
+//        try {
+//            MapsInitializer.initialize(this);
+//        } catch (GooglePlayServicesNotAvailableException e) {
+//            Log.d(TAG, "You must update Google Maps."); finish();
+//        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMyLocationEnabled(true);    //現在地に戻るボタン
 
-        //GPSから位置情報の取得
+        //スタンプラリーのスポット3点（デバッグ用）
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(34.684581, 135.526349)).title("大阪城"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(34.645842, 135.513971)).title("あべのハルカス"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(34.6671654, 135.4356473)).title("USJ"));
+
+
+        //プロバイダの取得
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        //GPSプロバイダの取得
-        String provider = LocationManager.GPS_PROVIDER;
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria. ACCURACY_FINE);       //高精度
+        criteria.setAccuracy(Criteria.NO_REQUIREMENT);
+
+        String provider = mLocationManager.getBestProvider(criteria, true);
 
         if(mLocationManager.isProviderEnabled(provider)){
             System.out.println("プロバイダ情報---------");
             System.out.println("取得したプロバイダ:"+provider);
         }
 
-        mLocationManager.requestLocationUpdates(provider, 5000, 10, this);
+        LatLng start_position = new LatLng(21.308889,-157.826111);
+        mMap.addMarker(new MarkerOptions()
+                .position(start_position)
+                .title("ホノルル")
+                .snippet("START_POSITION"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(start_position));
 
-
-        //マーカー
-        LatLng sydney = new LatLng(34.68639, 135.52);     //マーカーを大阪の位置情報で指定（仮）
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        //test
-        System.out.println("MAPテストSTART！！！！！！！！！！！！！！");
-        System.out.println("LatLngの内容を表示:"+ sydney.toString());
-        System.out.println("MAPテストFINISH！！！！！！！！！！！！！！");
+        mLocationManager.requestLocationUpdates(provider, 1, 1, this);
 
     }
+
+//    @Override
+//    public void OnMapClickListener(){
+//
+//    }
 
     @Override
     public void onStop() {
@@ -66,11 +92,18 @@ public class MapsActivity extends FragmentActivity implements LocationListener,O
         mLocationManager.removeUpdates(this);    // 位置情報の更新を止める
     }
 
-    @Override   //位置座標を取得出来たら引数に渡して呼び出される
+    @Override   //位置座標を取得したら引数に渡して呼び出される
     public void onLocationChanged(Location location) {
-        System.out.println("いぇああああああああああ");
-        System.out.println("緯度:" + location.getLatitude());
-        System.out.println("軽度:" + location.getLongitude());
+        //位置情報を取得したらダイアログを表示する
+//        new AlertDialog.Builder(this)
+//                .setTitle("GPS")
+//                .setMessage("位置情報を取得しました")
+//                .setPositiveButton("OK", null)
+//                .show();
+
+        //カメラを現在地に移動
+        LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
 
     }
 
